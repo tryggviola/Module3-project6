183629809ca3937a95e275557ccbd9acb3d6a2d7
Tryggvi <tryggviola@gmail.com>
2021-09-24 00:50:01 +0000

    adding loader

--- java.js
+++ java.js
@@ -1,3 +1,15 @@
+//loader for 2 seconds
+const loader = document.querySelector(".loader");
+
+window.onload = function () {
+	setTimeout(function () {
+		loader.style.opacity = "0";
+		setTimeout(function () {
+			loader.style.display = "none";
+		}, 500);
+	}, 2000);
+};
+
 // add item to shopping list table from input
 function addItem() {
 	// get value from input item and input qty
